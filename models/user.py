73dee68b11f3ed3190f890c0dd4494429cf43d1d
Noureddine Nassiri <nassir_n@etna-alternance.net>
2021-09-17 16:21:20 +0200

    Added aalembic and some models

---
+++ models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, DateTime, String, Integer, func  
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Users(Base):  
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    lastname = Column(String, unique=True)
+    firstname = Column(String)
+    email = Column(String)
+    phone = Column(String)
+    password = Column(String)
+
+    def __repr__(self):
+        return 'id: {}, root cause: {}'.format(self.id, self.root_cause)
